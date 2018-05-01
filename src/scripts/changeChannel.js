b0ebc3ceef874a4a4e52788813c270691daa4db6
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-05-01 12:15:05 -0500

    changing channels working

--- src/scripts/changeChannel.js
+++ src/scripts/changeChannel.js
@@ -1,14 +1,16 @@
 const changeChannel = (evt) => {
     const allChannels = document.querySelectorAll('.individual-channel');
-    const activeChannel = evt.path[0]
+    let clickedChannel = evt.target
 
+    if (!clickedChannel.id) {
+        clickedChannel = evt.path[1]
+    }
     for (let i = 0; i < allChannels.length; i++) {
         if (allChannels[i].classList.contains('activeChannel')) {
             allChannels[i].classList.remove('activeChannel');
         }
     }
-    activeChannel.classList.add('activeChannel');
-
+    clickedChannel.classList.add('activeChannel')
 }
 
 module.exports = changeChannel;
