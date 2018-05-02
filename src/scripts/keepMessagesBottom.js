146b6677e80cdaf4fefb9685e97121e7ce25848a
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-05-02 15:12:37 -0500

    stick to bottom of div on load works

--- src/scripts/keepMessagesBottom.js
+++ src/scripts/keepMessagesBottom.js
@@ -1,15 +1,8 @@
-// const keepMessagesBottom = () => {
-//     console.log('hi')
-//     let messageBody = document.querySelector('#messages');
-//     console.log(messageBody.scrollHeight)
-//     messageBody.scrollTop = 9999;
-// }
+const keepMessagesBottom = () => {
+    console.log('hi')
+    let messageBody = document.querySelector('#messages');
+    console.log(messageBody.scrollHeight)
+    messageBody.scrollTop = 9999;
+}
 
-// window.onload = function(){
-//     setTimeout(function(){
-//         console.log('hi')
-//         keepMessagesBottom();
-//     }, 200);
-//  };
-
-// module.exports = keepMessagesBottom;
+module.exports = keepMessagesBottom;
