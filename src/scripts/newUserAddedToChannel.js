54af64827019b0bb3682d755c2700be0a579933c
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-05-03 12:50:10 -0500

    user added to channel notification working

--- src/scripts/newUserAddedToChannel.js
+++ src/scripts/newUserAddedToChannel.js
@@ -1,6 +1,5 @@
 const newUserAddedToChannel = (user) => {
     const dateGenerator = require('./dateGenerator');
-    const messageFactory = require('./messageFactory');
     const postArea = document.querySelector('.messages');
     const addMessageToChannel = require('./databaseAddMessage').addMessageToChannel;
     const addMessage = require('./databaseAddMessage').addMessage;
@@ -17,10 +16,53 @@ const newUserAddedToChannel = (user) => {
         text,
         media
     }
-    const messageStructure = messageFactory(date, newMessage)
+    const messageStructure = createNotification(date, newMessage)
     postArea.appendChild(messageStructure);
     addMessageToChannel(newMessage)
     addMessage(newMessage)
+}
+
+const createNotification = (date, message) => {
+    const messageStructure = document.createElement('span');
+    const body = document.createElement('span');
+    body.classList.add('message__right')
+    const title = notificationTitle(date, message.user.email);
+    const avatar = notificationAvatar();
+    messageStructure.classList.add('message')
+    const text = document.createElement('p');
+
+    text.classList.add('message__body', 'message__newUser');
+    text.textContent = message.text;
+    
+    body.appendChild(title)
+    body.appendChild(text)
+    
+    messageStructure.appendChild(avatar)
+    messageStructure.appendChild(body);
+
+    return messageStructure;
+}
+
+const notificationTitle = (date, user) => {
+    const messageTitle = document.createElement('span');
+    messageTitle.classList.add('message__title')
+    const displayName = document.createElement('p')
+    displayName.textContent = user;
+    const postDate = document.createElement('p')
+    postDate.textContent = date;
+    displayName.classList.add('message__title--user');
+    postDate.classList.add('message__title--date');
+    messageTitle.appendChild(displayName)
+    messageTitle.appendChild(postDate)
+    
+    return messageTitle;
+}
+
+const notificationAvatar = () => {
+    const avatar = document.createElement('img');
+    avatar.src = 'img/avatar.png'
+    avatar.classList.add('messages__avatar')
+    return avatar
 }
 
 module.exports = newUserAddedToChannel;
