4c9789426412c886f75d231d1167971bd4ff6f2a
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-05-02 20:59:14 -0500

    refactored scrolling feature

--- src/scripts/keepMessagesBottom.js
+++ src/scripts/keepMessagesBottom.js
@@ -1,7 +1,5 @@
 const keepMessagesBottom = () => {
-    console.log('hi')
     let messageBody = document.querySelector('#messages');
-    console.log(messageBody.scrollHeight)
     messageBody.scrollTop = 9999;
 }
 
